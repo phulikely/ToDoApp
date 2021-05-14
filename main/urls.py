ed21fa59ae56202e5bde49a07d3c746ca7947a20
phulikely <phulikely@gmail.com>
2021-05-14 11:32:06 +0700

    adding User Specific Data

--- main/urls.py
+++ main/urls.py
@@ -7,6 +7,9 @@ urlpatterns = [
     path('login/', views.login_page, name='login'),
     path('logout/', views.logout_page, name='logout'),
     path('register/', views.register_page, name='register'),
+
+    path('create_task/', views.create_task, name='create_task'),
+    path('task/<str:pk>/', views.detail_task, name='detail_task'),
     path('update_task/<str:pk>/', views.update_task, name='update_task'),
     path('delete_task/<str:pk>/', views.delete_task, name='delete_task'),
 ]
--- main/views.py
+++ main/views.py
@@ -1,22 +1,27 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 from .models import *
 from .forms import *
 
 
 def login_page(request):
-    if request.method == 'POST':
-        username = request.POST.get('username')
-        password = request.POST.get('password')
-        user = authenticate(request, username=username, password=password)
-        if user is not None:
-            login(request, user)
-            return redirect('home')
-        else:
-            messages.info(request, 'Username or Password is incorrect')
-    context = {}
-    return render(request, 'main/login.html', context)
+    if request.user.is_authenticated:
+        return redirect('home')
+    else:
+        if request.method == 'POST':
+            username = request.POST.get('username')
+            password = request.POST.get('password')
+            user = authenticate(request, username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect('home')
+            else:
+                messages.info(request, 'Username or Password is incorrect')
+        context = {}
+        return render(request, 'main/login.html', context)
 
 
 def logout_page(request):
@@ -25,38 +30,67 @@ def logout_page(request):
 
 
 def register_page(request):
-    form = CreateUserForm()
-    if request.method == 'POST':
-        form = CreateUserForm(request.POST)
-        if form.is_valid():
-            user = form.save()
-            username = form.cleaned_data.get('username')
-            # group = Group.objects.get(name='client')
-            # user.groups.add(group)
-            # Customer.objects.create(user=user)
-            messages.success(request, 'Account created for ' + username + ' successfully')
-            return redirect('login')
-        else:
-            messages.error(request, 'Account created unsuccessfully')
-    context = {'form':form}
-    return render(request, 'main/register.html', context)
+    if request.user.is_authenticated:
+        return redirect('home')
+    else:
+        form = CreateUserForm()
+        if request.method == 'POST':
+            form = CreateUserForm(request.POST)
+            if form.is_valid():
+                user = form.save()
+                username = form.cleaned_data.get('username')
+                # group = Group.objects.get(name='client')
+                # user.groups.add(group)
+                # Customer.objects.create(user=user)
+                # messages.success(request, 'Account created for ' + username + ' successfully')
+                return redirect('login')
+            else:
+                messages.error(request, 'Account created unsuccessfully')
+        context = {'form':form}
+        return render(request, 'main/register.html', context)
 
 
+@login_required(login_url='login')
 def home(request):
-    tasks = Task.objects.all()
+    #tasks = Task.objects.all()
+    tasks = request.user.task_set.all()
+    #print('task theo user : ', tasks1)
 
     form = TaskForm()
-
-    if request.method == 'POST':
-        form = TaskForm(request.POST)
-        if form.is_valid():
-            form.save()
-        return redirect('home')
 
     context = {'tasks':tasks, 'form':form}
     return render(request, 'main/list.html', context)
 
 
+@login_required(login_url='login')
+def create_task(request):
+    #user = request.user
+    form = TaskForm()
+    if request.method == 'POST':
+        form = TaskForm(request.POST)
+        if form.is_valid():
+            # form.save()
+            data = dict.copy(form.cleaned_data)
+            data.update({'user': request.user})
+            Task.objects.create(**data)
+        return redirect('home')
+
+    context = {'form':form}
+    return render(request, 'main/create_task.html', context)
+
+
+@login_required(login_url='login')
+def detail_task(request, pk):
+    task = Task.objects.get(id=pk)
+
+    form = TaskForm(instance=task)
+
+    context = {'form':form}
+
+    return render(request, 'main/detail_task.html', context)
+
+
+@login_required(login_url='login')
 def update_task(request, pk):
     task = Task.objects.get(id=pk)
 
@@ -73,6 +107,7 @@ def update_task(request, pk):
     return render(request, 'main/update_task.html', context)
 
 
+@login_required(login_url='login')
 def delete_task(request, pk):
     item = Task.objects.get(id=pk)
 
